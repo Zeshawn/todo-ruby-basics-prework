0b0ee8e5bac3d6b28b549d29fbdebb25a1e9ac88
Zeshawn Khan <zeshawnk@gmail.com>
2018-08-22 22:39:31 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -14,9 +14,11 @@ def greeting(greeting, name)
  "#{greeting} #{name}"
 end
 
-def return_a_value
-
+def return_a_value(phrase)
+  "#{phrase}"
 end
+
+return_a_value("Nice")
 
 def last_evaluated_value
 
